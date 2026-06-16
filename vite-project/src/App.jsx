429848c592603dbbd2c0494a029663534967f39d
Your Name <your@email.com>
2026-06-16 10:00:12 +0530

    Add all bootcamp projects

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from './Home'
+
+const App = () => {
+  return (
+    <>
+    <div>App</div>
+    <h1> I HATE MANGO MANGO </h1>
+    <Home/>
+    </>
+  )
+}
+
+export default App
